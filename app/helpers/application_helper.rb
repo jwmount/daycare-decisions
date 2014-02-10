3e2c7e1c8b7e4567ce187f8cf03fdb03f54925cc
John Mount <john@venuesoftware.com>
2014-02-10 09:57:37 -0800

    v0.1.2.  ApplicationHelper from model.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
-end
+# Care Categories
+def care_categories
+ [
+  'Long Day Care',
+  'Occasional Care',
+  'Family Day Care',
+  'In Home Care',
+  'Before and After School Care',
+  'Vacation Care',
+  # current child care enquiry may find they need to provide a rebate to nannies and aupairs, 
+  # we need to scope the flexibility in the major build to include this)
+  'Future scope- nannies, babysitters, aupairs, ...'
+  ]
+  end
+
+
+  def address
+    @address = Address.where("addressable_id = ? AND addressable_type = ?", self.id, 'Company').limit(1)
+    unless @address.blank?
+      address = "#{@address[0].street},  #{@address[0].suburb} #{@address[0].state} #{@address[0].post_code} "
+    else
+      'Empty'
+    end
+  end
+
+
+end #module
