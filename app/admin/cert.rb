ceb5244b0e5aa2f20adf423904ecc2c781161de8
John Mount <john@venuesoftware.com>
2014-02-13 12:37:28 -0800

    v0.1.18.  Certs show Certificates.  Do not record expires_on when created directly (but we may never do that, rather create as edit on Provider.

--- app/admin/cert.rb
+++ app/admin/cert.rb
@@ -2,6 +2,5 @@ ActiveAdmin.register Cert do
 
   menu parent: "Compliance"
 
-  permit_params :expires_on, :active
-  
+  permit_params :expires_on, :active, :certificate_id, :certifiable_type, :serial_number  
 end
