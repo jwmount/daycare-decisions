14161fd9f295c3e76cc16ebe1173f3aae7be6ae8
John Mount <john@venuesoftware.com>
2014-02-14 16:20:32 -0800

    deployment to Heroku, missing ability.rb for some reason.  Deleted word 'ability' at line 1 on models ability.rb.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,3 @@
-ability.rb
 # The first argument to `can` is the action you are giving the user permission to do.
 # If you pass :manage it will apply to every action. Other common actions here are
 # :read, :create, :update and :destroy.
