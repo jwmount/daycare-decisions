1c6acf3e7f13d0b9f1d101ec2b231dbc23bb0496
John Mount <john@venuesoftware.com>
2014-02-18 08:11:09 +1000

    v0.1.21.  Addresses can be used to locate facilities.  Neat.

--- app/admin/address.rb
+++ app/admin/address.rb
@@ -1,7 +1,35 @@
 ActiveAdmin.register Address do
 
   menu parent: "Admin"
-  
+
+  actions :all, :except => :new
+
+  index do
+
+    selectable_column
+
+    column "Name (click for details)", :sortable => 'name' do |address|
+      facility = "#{address.addressable_type}".constantize.where(id: address.addressable_id)
+      render facility
+    end
+
+    column "Kind of Facility" do |address|
+    	address.addressable_type
+    end
+    column :street
+    column :suburb
+    column :state
+    column :post_code
+    column :lat
+    column :long
+    column :created_at
+    column :updated_at
+
+  end
+#
+# P A R A M S
+#  
+
   permit_params :street,  :suburb, :state, :post_code, :lat, :long, :utf8
   
 end
