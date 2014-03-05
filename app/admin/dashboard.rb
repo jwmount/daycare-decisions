a9423fbb0b80a4d48618330f2942a7dbe5de52ed
John Mount <john@venuesoftware.com>
2014-03-05 18:04:27 +1000

    v0.1.47.  Removed welcome msgs since can't change them.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,22 +2,26 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
+
   content :title => proc{ I18n.t("active_admin.dashboard") } do
+=begin
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
       span :class => "blank_slate" do
         span I18n.t("active_admin.dashboard_welcome.welcome")
         small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
-
+=end
     # Here is an example of a simple dashboard with columns and panels.
     #
      columns do
+
        column do
-         panel "Selected Providers" do
+         panel "Five Most Recently Updated Providers" do
            ul do
              Provider.order("updated_at DESC").limit(5).map do |provider|
-               li link_to("#{provider.name}, #{provider.addresses[0].locality}", admin_provider_path(provider))
+               label = "#{provider.name}, #{provider.addresses[0].locality}, #{provider.addresses[0].state}"
+               li link_to(label, admin_provider_path(provider))
              end
            end
          end
@@ -27,7 +31,7 @@ ActiveAdmin.register_page "Dashboard" do
          panel "Providers With Vacancies" do
            ul do
              Provider.where(:vacancies => :true).count 
-             end
+           end
          end
        end
 
