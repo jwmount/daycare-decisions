d5d1f3fe3b042b70ae2b78f4e911dfb8498a1ce7
John Mount <john@venuesoftware.com>
2014-03-07 08:48:37 +1000

    quick fix, remove parameter list in Companies controller.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,9 +4,5 @@ class CompaniesController < InheritedResources::Base
 # P A R A M S
 # 
 
-  permit_params :name, :active, :url, :description,
-      addresses_attributes: [:street_address, :locality, :state, :post_code, :lat, :long, :_destroy],
-      rolodexes_attributes: [:number_or_email, :kind, :when_to_use, :description, :_destroy],
-      certs_attributes: [ :certificate_id, :serial_number, :expires_on, :active, :_destroy ]
 
 end
