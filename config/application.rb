2fe0e8877435d5552eb54eac9b85b6b53704689c
John Mount <john@venuesoftware.com>
2014-03-10 08:19:31 +1000

    v0.1.50x.  Sprockets problems.  Removded config.serve_static_assets = truefrom config application.rb.  Let grem rails_12factor do it.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,5 @@ module DaycareDecisions
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Tell Heroku to serve assets, default is false
-    config.serve_static_assets = true
   end
 end
