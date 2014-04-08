5b18c733e32337161f482d1091cad40cc51785c5
John Mount <john@venuesoftware.com>
2014-04-07 19:18:30 -0700

    v0.1.68.  Lottsa layout on index page now, can write list of providers into it, but not much logic in choosing them yet.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
+
   def index
+  	@addresses = Address.where(locality: "BRISBANE")
+    @providers = []
+    @addresses.each do |a|
+    	@providers << Provider.where(id: a.addressable_id)
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @companies }
+    end
   end
+
+  def edit
+  	debugger
+  end
+
+  def update
+  	debugger
+  end
+
 end
--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-ENV['VERSION'] = "v0.1.67. &copy Copyright Daycare Decisions, DBA, 2014."
+ENV['VERSION'] = "v0.1.68. &copy Copyright Daycare Decisions, DBA, 2014."
