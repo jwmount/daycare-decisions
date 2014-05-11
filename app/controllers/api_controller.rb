96284cda3e2764b810d96456529e361db8de1157
John Mount <john@venuesoftware.com>
2014-05-10 18:04:53 -0700

    v0.1.106.  Added before_filter to set access control  headers in api_controller.  locally makes no difference so this is pushed so Sean can test it for the new front end.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,11 @@
 # localhost:3000/api => all providers
-# localhost:3000/api/20 => provider :id = 20
+# localhost:3000/api/locations/bri => locations with 'b-r-i' prefix
 # http://localhost:3000/api?utf8=%E2%9C%93&locality=burleigh%20wa&post_code=&state=&commit=Find+Address
 # curl -i -H "Accept: application/json" http://localhost:3000/api/1  
 
 class ApiController < ApplicationController
   include ActionController::MimeResponds
+  after_filter :set_access_control_headers
 
   #
   # Get all locations in soundex queries, e.g. 'B' followed by 'Bri'
@@ -50,6 +51,11 @@ class ApiController < ApplicationController
     render :json => Provider.where( "id = ?", params[:id])
   end
 
-
+  # from http://madhukaudantha.blogspot.com/2011/05/access-control-allow-origin-in-rails.html
+  # called by before filter
+  def set_access_control_headers 
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:8081/' 
+    headers['Access-Control-Request-Method'] = '*' 
+  end
 
 end
