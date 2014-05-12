c4ced5dba9ea26d724879f44079081e3cbfcdd7b
Sean Schertell <sean@datafly.net>
2014-05-12 16:12:59 -0700

    Autocomplete is working

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -14,71 +14,25 @@ app.config(['$httpProvider', function($httpProvider) {
 
 // Locations Controller
 app.controller("locationsCtrl", function($scope, $http) {
+var locations_api_call = server + "/locations/";
 
-    updateLocations = function() { alert('hi') };
+    $scope.updateLocations = function() {
+        if ($scope.selectedLocation.length < 2 ){
+            return;
+        }
+        $http
+            .get(locations_api_call + String($scope.selectedLocation))
+            .success(function(data) {
+                $scope.locations = data;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log(data)
+            });
+        };
 
-    locations_api_call = server + "/locations/a";
-    console.log(locations_api_call);
-    $http
-        .get(locations_api_call)
-        .success(function(data) {
-            $scope.locations = data;
-            console.log(data);
-        })
-        .error(function (data) {
-            console.log(data)
-        });
 
-    // $scope.locations = [
-    //     "Adelaide, SA",
-    //     "Mount Barker, SA",
-    //     "Mount Gambier, SA",
-    //     "Murray Bridge, SA",
-    //     "Port Adelaide, SA",
-    //     "Port Augusta, SA",
-    //     "Port Pirie, SA",
-    //     "Port Lincoln, SA",
-    //     "Victor Harbor, SA",
-    //     "Whyalla, SA",
-    //     "Brisbane, QLD",
-    //     "Bundaberg, QLD",
-    //     "Cairns, QLD",
-    //     "Caloundra, QLD",
-    //     "Gladstone, QLD",
-    //     "Gold Coast, QLD",
-    //     "Gympie, QLD",
-    //     "Hervey Bay, QLD",
-    //     "Ipswich *, QLD",
-    //     "Logan City *, QLD",
-    //     "Mackay, QLD",
-    //     "Maryborough, QLD",
-    //     "Mount Isa, QLD",
-    //     "Rockhampton, QLD",
-    //     "Sunshine Coast, QLD",
-    //     "Toowoomba, QLD",
-    //     "Townsville, QLD",
-    //     "Sydney, NSW",
-    //     "Albury, NSW",
-    //     "Armidale, NSW",
-    //     "Bathurst, NSW",
-    //     "Blue Mountains, NSW",
-    //     "Broken Hill, NSW",
-    //     "Campbelltown *, NSW",
-    //     "Cessnock, NSW",
-    //     "Dubbo, NSW",
-    //     "Goulburn, NSW",
-    //     "Grafton, NSW",
-    //     "Lithgow, NSW",
-    //     "Liverpool *, NSW",
-    //     "Newcastle, NSW",
-    //     "Orange, NSW",
-    //     "Parramatta *, NSW",
-    //     "Penrith *, NSW",
-    //     "Queanbeyan, NSW",
-    //     "Tamworth, NSW",
-    //     "Wagga Wagga, NSW",
-    //     "Wollongong, NSW"
-    // ];
+
 
     // --> https://github.com/JustGoscha/allmighty-autocomplete
 
