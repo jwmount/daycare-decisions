a9b75c031d96bea777b25c25d1fe156748dd1987
Sean Schertell <sean@datafly.net>
2014-05-12 12:33:45 -0700

    Continuing efforts to connect front-end to server (workaround CORS issues). Simple devserver added.

---
+++ frontend/_server/devserver.py
@@ -0,0 +1,17 @@
+from os.path import join, dirname, abspath
+from bottle import response, run, get, debug, static_file
+
+app_path = abspath( join( dirname(__file__), '..' ) )
+ #www_path = join(app_path, 'www')
+
+@get('/')
+def index():
+    return static_file("index.html", root=app_path)
+
+
+@get('/<filename:path>')
+def static(filename):
+    return static_file(filename, root=app_path)
+
+
+run(host='localhost', port=8081, debug=True, reloader=True)
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,17 +1,86 @@
 // Set Config Variables
-var server = "http://localhost:8080/providers/1";
+// var server = "http://localhost:8080/providers/1";
+var server = "http://daycare-decisions.herokuapp.com/api";
 
 // Initialize Angular App
-var app = angular.module("app", ['autocomplete']);
+var app = angular.module("app", ['autocomplete', 'ngSanitize']);
 
-// Providers Controller
-app.controller("ProvidersCtrl", function($scope, $http) {
-    $scope.locations = [123,234,345,456,567,678];
+// app.config(['$httpProvider', function($httpProvider) {
+//         $httpProvider.defaults.useXDomain = true;
+//         delete $httpProvider.defaults.headers.common['X-Requested-With'];
+//     }
+// ]);
+
+
+// Locations Controller
+app.controller("locationsCtrl", function($scope, $http) {
+
+    updateLocations = function() { alert('hi') };
+
+    locations_api_call = server + "/locations/a";
+    console.log(locations_api_call);
+    $http
+        .get(locations_api_call)
+        .success(function(data) {
+            $scope.locations = data;
+            console.log(data);
+        })
+        .error(function (data) {
+            console.log(data)
+        });
+
+    // $scope.locations = [
+    //     "Adelaide, SA",
+    //     "Mount Barker, SA",
+    //     "Mount Gambier, SA",
+    //     "Murray Bridge, SA",
+    //     "Port Adelaide, SA",
+    //     "Port Augusta, SA",
+    //     "Port Pirie, SA",
+    //     "Port Lincoln, SA",
+    //     "Victor Harbor, SA",
+    //     "Whyalla, SA",
+    //     "Brisbane, QLD",
+    //     "Bundaberg, QLD",
+    //     "Cairns, QLD",
+    //     "Caloundra, QLD",
+    //     "Gladstone, QLD",
+    //     "Gold Coast, QLD",
+    //     "Gympie, QLD",
+    //     "Hervey Bay, QLD",
+    //     "Ipswich *, QLD",
+    //     "Logan City *, QLD",
+    //     "Mackay, QLD",
+    //     "Maryborough, QLD",
+    //     "Mount Isa, QLD",
+    //     "Rockhampton, QLD",
+    //     "Sunshine Coast, QLD",
+    //     "Toowoomba, QLD",
+    //     "Townsville, QLD",
+    //     "Sydney, NSW",
+    //     "Albury, NSW",
+    //     "Armidale, NSW",
+    //     "Bathurst, NSW",
+    //     "Blue Mountains, NSW",
+    //     "Broken Hill, NSW",
+    //     "Campbelltown *, NSW",
+    //     "Cessnock, NSW",
+    //     "Dubbo, NSW",
+    //     "Goulburn, NSW",
+    //     "Grafton, NSW",
+    //     "Lithgow, NSW",
+    //     "Liverpool *, NSW",
+    //     "Newcastle, NSW",
+    //     "Orange, NSW",
+    //     "Parramatta *, NSW",
+    //     "Penrith *, NSW",
+    //     "Queanbeyan, NSW",
+    //     "Tamworth, NSW",
+    //     "Wagga Wagga, NSW",
+    //     "Wollongong, NSW"
+    // ];
 
     // --> https://github.com/JustGoscha/allmighty-autocomplete
 
-    // $http.get(server)
-    // .success(function(data) {
-    //     console.log(data);
-    // });
+
 });
