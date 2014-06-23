38005f4be43bb3d3f3a08c2c98b534557e890ecf
John W. Mount <john@venuesoftware.com>
2014-06-23 11:11:10 -0700

    v0.2.0-pg.  Deployment to Ninefold with API frontend.

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,8 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-ENV['VERSION'] = "v0.1.134-mySql. &copy Copyright Daycare Decisions, DBA, 2014."
+#ENV['VERSION'] = "v0.1.134-mySql. &copy Copyright Daycare Decisions, DBA, 2014."
+ENV['VERSION'] = "v0.2.0-pg. &copy Copyright Daycare Decisions, DBA, 2014."
 
 # Google Client side Geocoding
 # https://maps.googleapis.com/maps/api/geocode/json?address=5+St+Kilda+Ave.,+Broad+Beach,QLD,Postal_code+4218,Australia,&sensor=false&key=AIzaSyDVUWaiCEzOlXjYsSCJaKlAOwKcnqDA7Cs
