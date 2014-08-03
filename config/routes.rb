d06dea4ce17254e043fbcb55d70fd37a90cde384
John W. Mount <john@venuesoftware.com>
2014-08-02 18:44:06 -0700

    v0.2.11.  Replaced routes based on namespace 'feui'. This is how we do the AngularJS view from views frontend feui.html.erb. Duh.

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,11 @@ DaycareDecisions::Application.routes.draw do
 
   root to: "home#index"
 
+  #root to: "home#index"
+
+  root to: "frontend#feui"
+  get '*path', to: 'frontend#feui'
+
   # catch-all route for api_controller
   # http://www.tsheffler.com/blog/?p=428 -- note by Forrest Zeisler
   # get "*path" => "api#xss_options_request", :constraints => {:method => "OPTIONS"}
